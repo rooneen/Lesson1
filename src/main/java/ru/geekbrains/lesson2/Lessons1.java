6ba89fcc57090f6e00004b7cbbb647c56b879903
aleksey <davaleks82@mail.ru>
2021-02-25 23:46:42 +0300

    Initial commit

--- src/main/java/ru/geekbrains/lesson2/Lessons1.java
+++ src/main/java/ru/geekbrains/lesson2/Lessons1.java
@@ -3,7 +3,7 @@ package ru.geekbrains.lesson2;
 
 public class Lessons1 {
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
 
         int[] a = {1, 0, 1, 0, 0, 1};
         for (int i = 0; i < a.length; i++) {
@@ -15,19 +15,19 @@ public class Lessons1 {
 
         }
         System.out.println(a[0] + " " + a[1] + " " + a[2] + " " + a[3] + " " + a[4] + " " + a[5]);
-    }*/
+    }
 
 
-    /*public static void main(String[] args) {
+    static void createArray() {
         int[] a = new int[8];
 
         for (int i = 0, j = 0; i < a.length; i++, j += 3) {
             a[i] = j;
             System.out.print(a[i] + " ");
         }
-    }*/
+    }
 
-    /*public static void main(String[] args) {
+    static void changeArray2() {
         int[] a = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
         for (int i = 0; i < a.length; i++) {
             if (a[i] <= 6) {
@@ -35,9 +35,9 @@ public class Lessons1 {
             }
         }
         System.out.println(a[0] + " " + a[1] + " " + a[2] + " " + a[3] + " " + a[4] + " " + a[5]);
-    }*/
+    }
 
-    public static void main(String[] args) {
+    static void createArray2() {
         int length = 4  ; // length of array
 
         int[][] arr = new int[length][length];
@@ -52,4 +52,6 @@ public class Lessons1 {
     }
 
 }
+
+
 
