f8af2127985bd8dfd56608d0d84a6334ae1bb55f
aleksey <davaleks82@mail.ru>
2021-02-25 23:39:51 +0300

    Initial commit

--- src/main/java/ru/geekbrains/lesson1/Lesson3.java
+++ src/main/java/ru/geekbrains/lesson1/Lesson3.java
@@ -1,3 +1,5 @@
+package ru.geekbrains.lesson1;
+
 import java.util.Scanner;
 
 public class Lesson3 {
@@ -9,13 +11,24 @@ public class Lesson3 {
         byte a = 120;
         short b = 15155;
         int c = 444555255;
-        long d = 515622611;
+        long d = 51562444662611L;
         float e = 56.25f;
         double db = 225.36;
         char ch = 'u';
         boolean bu = true;
 
     }
+
+        //метод a + b >10 b <20
+
+        public static boolean task10and20(int a, int b){
+            if (a + b >= 10 && a + b <= 20) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
 
     {
 
@@ -60,8 +73,18 @@ public class Lesson3 {
 
     }
 
+    //2020 делится на 4 и не делится на 100
 
 
+    public static void year(int year){
+        if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0){
+            System.out.println("Год не високосный");
+        } else if (year % 4== 0){
+            System.out.println("Год високосный");
+        } else {
+            System.out.println("Год не високосный");
+        }
+    }
 
 
 }
