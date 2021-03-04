03a340f49bd64dea6e43be9bf0681464f0cb1953
aleksey <davaleks82@mail.ru>
2021-03-04 22:37:08 +0300

    Initial commit

--- src/main/java/ru/geekbrains/HomeWork2/Lessons1.java
+++ src/main/java/ru/geekbrains/HomeWork2/Lessons1.java
@@ -7,6 +7,7 @@ public class Lessons1 {
 
         int[] a = {1, 0, 1, 0, 0, 1};
         for (int i = 0; i < a.length; i++) {
+            // a[i] = a[i] == 0 ? 1 : 0; короткий метод
             if (a[i] == 0) {
                 a[i] = 1;
             } else {
